9bba0e7b7e906290c6eefef15931674fd7bd96bd
Siddhartha <siddhartha.pradhan.ix@gmail.com>
2024-01-31 18:17:04 +0545

    Change in Parameter Name.

--- API/Controllers/APIs/EBookController.cs
+++ API/Controllers/APIs/EBookController.cs
@@ -38,9 +38,9 @@ public class EBookController : Controller
     }
 
     [HttpPost("get-all-ebooks")]
-    public async Task<IActionResult> PostAllEbooks(int? classId, int? subjectCode, string? volume)
+    public async Task<IActionResult> PostAllEbooks(int? classId, int? subjectId, string? volume)
     {
-        var result = await _eBookService.GetAllEBooks(classId, subjectCode, volume);
+        var result = await _eBookService.GetAllEBooks(classId, subjectId, volume);
 
         var response = new ResponseDTO<List<EBookResponseDTO>>()
         {
