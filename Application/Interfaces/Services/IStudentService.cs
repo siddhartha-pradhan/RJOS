70a1f3936238d4f468900df32a42ecf9d049100e
Siddhartha <siddhartha.pradhan.ix@gmail.com>
2024-02-05 16:00:42 +0545

    Indentation Changes.

--- Application/Interfaces/Services/IStudentService.cs
+++ Application/Interfaces/Services/IStudentService.cs
@@ -6,7 +6,7 @@ public interface IStudentService
 {
     Task<StudentResponseDTO> GetStudentRecords(int studentId);
 
-
     Task InsertStudentResponse(List<StudentResponseRequestDTO> studentResponses);
+    
     Task InsertStudentScore(List<StudentScoreRequestDTO> studentScores);
 }
